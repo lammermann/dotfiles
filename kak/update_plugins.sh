f633e6ea06a24c171b2fe6efeb25ce1290dfac64
Benjamin Kober <kober@optisense.com>
2018-11-07 10:08:48 +0100

    Added Replace Mode Plugin

--- kak/update_plugins.sh
+++ kak/update_plugins.sh
@@ -11,10 +11,11 @@ cd `git rev-parse --show-toplevel`
 # from the git root and than add the following line to the script
 #git subtree pull -P kak/plugins/${repo_name} ${repo_url} master
 
-git subtree pull -P kak/plugins/kakoune-expand https://github.com/occivink/kakoune-expand.git
-git subtree pull -P kak/plugins/kakoune-find https://github.com/occivink/kakoune-find.git
-git subtree pull -P kak/plugins/kakoune-gdb https://github.com/occivink/kakoune-gdb.git
-git subtree pull -P kak/plugins/kakoune-phantom-selection https://github.com/occivink/kakoune-phantom-selection.git
-git subtree pull -P kak/plugins/kakoune-sudo-write https://github.com/occivink/kakoune-sudo-write.git
-git subtree pull -P kak/plugins/kakoune-vertical-selection https://github.com/occivink/kakoune-vertical-selection.git
+git subtree pull -P kak/plugins/kakoune-expand https://github.com/occivink/kakoune-expand.git master
+git subtree pull -P kak/plugins/kakoune-find https://github.com/occivink/kakoune-find.git master
+git subtree pull -P kak/plugins/kakoune-gdb https://github.com/occivink/kakoune-gdb.git master
+git subtree pull -P kak/plugins/kakoune-phantom-selection https://github.com/occivink/kakoune-phantom-selection.git master
+git subtree pull -P kak/plugins/kakoune-sudo-write https://github.com/occivink/kakoune-sudo-write.git master
+git subtree pull -P kak/plugins/kakoune-vertical-selection https://github.com/occivink/kakoune-vertical-selection.git master
+git subtree pull -P kak/plugins/replace.kak https://github.com/alexherbo2/replace.kak.git master
 
