d6d48bb02e6ef194d2e445cfc5d2eb794f7cb6c8
Benjamin Kober <kober@optisense.com>
2021-03-20 18:43:26 +0100

    Removed old install scripts

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Install all the config files in this repo to the right places
-ln -s ~/.config/tmux.conf ~/.tmux.conf
-ln -s ~/.config/bashrc ~/.bashrc
-ln -s ~/.config/bash_profile ~/.bash_profile
-ln -s ~/.config/Xdefaults ~/.Xdefaults
-ln -s ~/.config/inputrc ~/.inputrc
-ln -s ~/.config/nvim ~/.vim
-ln -s ~/.config/irbrc ~/.irbrc
