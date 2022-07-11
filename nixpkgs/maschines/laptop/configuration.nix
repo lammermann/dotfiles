2a9da266a9771cb34191987da3772c13cb20db6b
Benjamin Kober <kober@optisense.com>
2022-07-11 14:24:23 +0200

    task: make sure ports are blocked by default

--- nixpkgs/maschines/laptop/configuration.nix
+++ nixpkgs/maschines/laptop/configuration.nix
@@ -179,11 +179,10 @@ in {
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # enable firewall and block all ports
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [];
+  networking.firewall.allowedUDPPorts = [];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
