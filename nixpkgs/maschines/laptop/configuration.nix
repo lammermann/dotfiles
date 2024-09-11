5b03d96a6c9bfa0302c345f8eb789e8b2bf2ba9f
Benjamin Kober <benko@kober-systems.com>
2024-09-11 21:52:29 +0200

    removed kanata config as it interfered with HID development

--- nixpkgs/maschines/laptop/configuration.nix
+++ nixpkgs/maschines/laptop/configuration.nix
@@ -73,19 +73,6 @@ in {
       };
     };
   };
-  services.kanata = {
-    enable = true;
-    keyboards = {
-      internal = {
-        config = builtins.readFile ./us_internal_keyboard.kbd;
-        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
-      };
-      dell_de_usb = {
-        config = builtins.readFile ./dell_de_usb_keyboard.kbd;
-        devices = [ "/dev/input/by-id/usb-Dell_Dell_USB_Keyboard-event-kbd" ];
-      };
-    };
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
