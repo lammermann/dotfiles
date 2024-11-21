258b6f0286fe26b88069ebfb035795409fb1bbb3
Benjamin Kober <benko@kober-systems.com>
2024-11-21 09:07:38 +0200

    renamed deprecated keys

--- nixpkgs/maschines/pirin/configuration.nix
+++ nixpkgs/maschines/pirin/configuration.nix
@@ -43,9 +43,9 @@
   };
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "de";
-    xkbVariant = "nodeadkeys";
+    variant = "nodeadkeys";
   };
 
   services.gitea = {
